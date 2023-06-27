b739943d53db97db25d1e2c5ebe5280dc910a21b
rihabalyasiri <rihab.al-yasiri@web.de>
2023-06-27 21:22:22 +0200

    App finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sound_library/sound_library.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,12 +14,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Instrument(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Xylophone"),
+        ),
+        body: const SafeArea(child: Instrument()),
       ),
     );
   }
@@ -27,10 +30,36 @@ class MyApp extends StatelessWidget {
 class Instrument extends StatelessWidget {
   const Instrument({super.key});
 
+  void playWave(int waveNumber) {
+    SoundPlayer.i.playLocal("note$waveNumber.wav");
+  }
+
+  Expanded buildKey({color, waveNumber}) {
+    return Expanded(
+        child: ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(color),
+      ),
+      onPressed: () {
+        playWave(waveNumber);
+      },
+      child: null,
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        buildKey(color: Colors.redAccent, waveNumber: 1),
+        buildKey(color: Colors.tealAccent, waveNumber: 2),
+        buildKey(color: Colors.lightBlue, waveNumber: 3),
+        buildKey(color: Colors.purple, waveNumber: 4),
+        buildKey(color: Colors.yellowAccent, waveNumber: 5),
+        buildKey(color: Colors.black12, waveNumber: 6),
+        buildKey(color: Colors.deepOrange, waveNumber: 7),
+      ],
+    );
   }
 }
-
-
